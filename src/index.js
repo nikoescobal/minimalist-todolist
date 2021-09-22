db6951e0f2a81981cea43acde8c417e65ee53e3b
Niko Escobal <niko.escobal@gmail.com>
2021-09-23 03:13:36 +0800

    Edit package.json

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log("Hello, Webpack!");
 import './style.css';
+
+let arr = [];
+
+let tasksContainer = document.getElementById('tasks-container');
+tasksContainer.innerHTML = "hello";
+
+
+// let tasks = {
+//   description: ,
+//   completed: ,
+//   index:
+// };
